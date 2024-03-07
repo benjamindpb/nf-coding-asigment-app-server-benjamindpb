ff73ff6fc9da26d7ad644bf89150dd5e017f2e01
benjamindpb <benjamin.dpb@gmail.com>
2024-03-07 05:08:32 -0400

    feat: create API routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import projectsRoutes from "./routes/project.routes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(projectsRoutes);
 
 app.get('/', (_req, res) => res.send('Hello World!'));
 
